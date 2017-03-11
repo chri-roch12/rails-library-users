7f6056c03a3be35611c3887f95394df7db8cc895
chri-roch12 <chris.prochnow@gmail.com>
2017-03-10 16:18:46 -0800

    Added create user route, view, and redirect.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: "users#index"
 
-  get "/users/new" to: "users#new" as: "new_user" 
+  get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "users/:id", to: "user#show", as: "user"
 
 end
