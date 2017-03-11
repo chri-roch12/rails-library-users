1254e3ad21e7008cc0909bdfdff7abc9d3617bd7
chri-roch12 <chris.prochnow@gmail.com>
2017-03-11 14:04:23 -0800

    Added login, logout routes. Created logout button to profile page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
 
   root to: "users#index"
 
+#users stuff
   get "/users/new", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
   get "users/:id", to: "users#show", as: "user"
+
+#sessions stuff
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create"
 
 end
